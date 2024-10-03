41a28aa7913b8943400f50d693e913d73a5fe898
dzreal93 <dzreal_93@126.com>
2024-10-03 08:11:10 +0800

    feat：接入arco-design

--- entrypoints/sidepanel/App.vue
+++ entrypoints/sidepanel/App.vue
@@ -1,30 +1,101 @@
-<script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue';
-</script>
-
 <template>
-  <div>
-    <a href="https://wxt.dev" target="_blank">
-      <img src="/wxt.svg" class="logo" alt="WXT logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="WXT + Vue" />
+  <a-tabs class="mt10" type="rounded" size="medium">
+    <a-tab-pane key="1" title="起一个变量名">
+      <div>
+        <a-card :style="{ width: 'auto' }">
+          <a-space direction="vertical" size="small" fill>
+            <a-typographya>
+              <a-typography-title :heading="6">需求：</a-typography-title>
+              <a-textarea
+                placeholder="请输入你的需求"
+                :max-length="{ length: 10, errorOnly: true }"
+                allow-clear
+                show-word-limit
+              />
+            </a-typographya>
+
+            <a-typographya>
+              <a-typography-title :heading="6">编程语言：</a-typography-title>
+              <a-select
+                :style="{ width: '320px' }"
+                v-model="value"
+                placeholder="请选择编程语言"
+                allow-clear
+                allow-search
+              >
+                <a-option>Java</a-option>
+                <a-option>Python</a-option>
+                <a-option>TypeScript</a-option>
+                <a-option>Golang</a-option>
+              </a-select>
+            </a-typographya>
+
+            <a-typographya>
+              <a-typography-title :heading="6">变量用途：</a-typography-title>
+              <a-select
+                :style="{ width: '320px' }"
+                v-model="value"
+                placeholder="请选择变量用途"
+                allow-clear
+                allow-search
+              >
+                <a-option>Beijing</a-option>
+                <a-option>Shanghai</a-option>
+                <a-option>Guangzhou</a-option>
+                <a-option disabled>Disabled</a-option>
+              </a-select>
+            </a-typographya>
+
+            <a-button type="primary" size="small">开始起名</a-button>
+          </a-space>
+          <a-space direction="vertical" size="small" fill>
+            <a-typographya>
+              <a-typography-title :heading="6">生成结果</a-typography-title>
+              <a-typography-paragraph copyable>
+                Click the icon to copy this text.
+              </a-typography-paragraph>
+            </a-typographya>
+          </a-space>
+        </a-card>
+      </div>
+    </a-tab-pane>
+    <a-tab-pane key="2" title="变量名转换">
+      <a-card :style="{ width: 'auto' }">
+        <div>
+          <a-card :style="{ width: 'auto' }">
+            <a-space direction="vertical" size="small" fill>
+              <a-typographya>
+                <a-typography-title :heading="6"
+                  >输入变量名：</a-typography-title
+                >
+                <a-input
+                  :style="{ width: '320px' }"
+                  default-value="content"
+                  placeholder="请输入一个变量名"
+                  allow-clear
+                />
+              </a-typographya>
+              <a-button type="primary" size="small">开始转换</a-button>
+            </a-space>
+            
+            <a-space direction="vertical" size="small" fill>
+              <a-typographya>
+                <a-typography-title :heading="6">生成结果</a-typography-title>
+                <a-typography-paragraph copyable>
+                  Click the icon to copy this text.
+                </a-typography-paragraph>
+              </a-typographya>
+            </a-space>
+          </a-card>
+        </div>
+      </a-card>
+    </a-tab-pane>
+    <a-tab-pane key="3" title="变量名优化">
+      <a-card :style="{ width: 'auto' }"> 施工中 </a-card>
+    </a-tab-pane>
+  </a-tabs>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #54bc4ae0);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script lang="ts" setup></script>
+
+<style scoped></style>
